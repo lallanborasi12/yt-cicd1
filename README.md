1628878359f13b027a46d6a16fff825ddaa98d0a
lallanborasi12 <lallan.kumar@wisepromo.com.com>
2026-06-22 15:36:31 +0530

    my commit 12

--- README.md
+++ README.md
@@ -9,5 +9,5 @@ npm install && npm run dev
 **Now All Set you can open below url to see your page**
 
 ```js 
-http://localhost:8000
+http://localhost:5000
 ```
--- src/index.ts
+++ src/index.ts
@@ -3,7 +3,7 @@ import type { Application, Request, Response } from "express";
 import "dotenv/config";
 import cors from "cors";
 const app: Application = express();
-const PORT = process.env.PORT || 7000;
+const PORT = process.env.PORT || 3000;
 
 // * Middleware
 app.use(cors());
@@ -11,10 +11,8 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
 app.get("/", (req: Request, res: Response) => {
-  return res.send("It's working 🙌");
+  return res.send("It's working @@ 🙌");
 });
-
-
 
 
 app.listen(PORT, () => console.log(`Server is running on PORT ${PORT}`));
